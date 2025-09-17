d67d30cc7a8444f1841c8b265421844e430fad05
Nhat Anh <91367670+NABlackFox@users.noreply.github.com>
2025-09-17 15:15:54 +0700

    asdf

--- Test Suites/UserCreateEvent.ts
+++ Test Suites/UserCreateEvent.ts
@@ -11,4 +11,18 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>788aed8f-3eb3-4cfe-beec-4fe9d3d5e694</testSuiteGuid>
+   <testCaseLink>
+      <guid>105c0784-2432-4d78-b792-161c795f83eb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UserCreateEvent/TC_User_Create_New_Event_1_ValidEvent</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4e13b19b-5924-40da-84a5-8a24d29bdf60</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UserCreateEvent/TC_User_Create_New_Event_2_InvalidEvent</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
--- Test Suites/UserEditEvent.ts
+++ Test Suites/UserEditEvent.ts
@@ -11,4 +11,18 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>a3472905-b85f-4892-bb52-7180f0def32f</testSuiteGuid>
+   <testCaseLink>
+      <guid>81a1d771-61d6-4d6d-8af6-85df3794ea8b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UserUpdateEvent/TC_User_Update_Event_1_ChangeEventName</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f3fc53f1-2b9d-4663-8e66-8bdbe2c3902d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UserUpdateEvent/TC_User_Update_Event_2_DeleteEvent</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
